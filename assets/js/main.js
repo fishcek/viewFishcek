b1ea31062cf3c4221b4cbf4431bf6821aa40a490
fishcek <info.fishcek@gmail.com>
2021-02-07 23:04:45 +0700

    Rubah login dan register

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,7 +121,7 @@
 
   // Toggle .header-scrolled class to #header when page is scrolled
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
+    if ($(this).scrollTop() > 0) {
       $('#header').addClass('header-scrolled');
     } else {
       $('#header').removeClass('header-scrolled');
